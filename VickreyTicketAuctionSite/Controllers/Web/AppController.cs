aaa7d3b72c58772966baf018d621dd49879b51f7
Samuel Thompson <nevdka@gmail.com>
2017-03-15 23:58:08 +1100

    First commit. It's late, and I could only get the project set up...

---
+++ VickreyTicketAuctionSite/Controllers/Web/AppController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VickreyTicketAuctionSite.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public AppController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- VickreyTicketAuctionSite/Startup.cs
+++ VickreyTicketAuctionSite/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +17,7 @@ namespace VickreyTicketAuctionSite
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +30,16 @@ namespace VickreyTicketAuctionSite
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvc( config =>
+             {
+                 config.MapRoute(
+                     name: "default",
+                     template: "{controller}/{action}/{id?}",
+                     defaults: new { controller = "App", action = "Index" }
+                     );
+             } );
+
         }
     }
 }
